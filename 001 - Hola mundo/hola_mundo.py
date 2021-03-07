8e62195a5b24e59c442ecac7e9971d3c4c4840d1
Wilebaldo Santana Hernández <wisahe@gmail.com>
2021-03-07 13:30:46 -0700

    Hola mundo!

---
+++ 001 - Hola mundo/hola_mundo.py
@@ -0,0 +1,12 @@
+# Iniciamos, como es tradición en muchos lenguajes,
+# con nuestro primer programa Hola mundo!, pero en
+# versión en mexicano. :D
+# Lo que está escrito después de la almohadilla o
+# hastag, no se ejecuta porque es un comentario.
+
+# La función print sirve para imprimir en pantalla.
+# Para ejecutar el programa, escribimos lo siguiente
+# en la terminal:
+# $ python3 hola_mundo.py
+# No es necesario escribir el símbolo $
+print("Quiúbole mundo!")
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python_learning
-Repositorio con ejercicios para aprender Python
+Repositorio con ejercicios para aprender Python.
+
+Evaluaremos varios proyectos para ir aprendiendo de una manera más amena. Estarán dentro de directorios numerados, así te será más fácil ubicarte dentro del tópico de tu interés.
